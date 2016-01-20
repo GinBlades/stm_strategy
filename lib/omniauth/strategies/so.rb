812652bc1cdf4c33d3373f8826d95142697f4345
Shaun Martin <webmaster@ginblades.com>
2016-01-20 09:27:49 -0600

    TODO: Update generator for Rails secrets.

--- lib/omniauth/strategies/so.rb
+++ lib/omniauth/strategies/so.rb
@@ -4,15 +4,6 @@ module OmniAuth
     class So < OmniAuth::Strategies::OAuth2
       option :name, "so"
 
-      CUSTOM_PROVIDER_URL = ENV['AUTH_PROVIDER_URL'] || "http://custom-provider-goes-here"
-      CUSTOM_PROVIDER_ME_URL = ENV['AUTH_PROVIDER_ME_URL'] || "/oauth/me.json"
-
-      option :client_options, {
-        site:  CUSTOM_PROVIDER_URL,
-        authorize_url: "#{CUSTOM_PROVIDER_URL}/oauth/authorize",
-        access_token_url: "#{CUSTOM_PROVIDER_URL}/oauth/token"
-      }
-
       uid {
         raw_info['id'] 
       }
@@ -34,7 +25,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get(CUSTOM_PROVIDER_ME_URL).parsed
+        @raw_info ||= access_token.get("/oauth/me.json").parsed
       end
 
       def callback_url
